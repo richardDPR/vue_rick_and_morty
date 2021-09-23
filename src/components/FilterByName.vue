f6dfc30dae48410c6fa130ef5d6cc1e90399056f
richardDPR <89047825+richardDPR@users.noreply.github.com>
2021-09-23 19:39:10 -0400

    mi proyecto vue

---
+++ src/components/FilterByName.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="buscar">
+      <input type="text" placeholder="Buscar por nombre" v-model="name"  @keyup="filter()">
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { useStore } from 'vuex'
+export default {
+    setup(){
+        const store = useStore()
+        const name = ref('')
+
+        const filter = () => {
+            store.dispatch('filterByName', name.value)
+        }
+
+        return{
+            name,
+            filter
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.buscar {
+    width: 400px;
+    margin: 3rem auto 0;
+    input {
+        height: 53px;
+        width: 400px;
+        border: none;
+        border-radius: 10px;
+        padding: 0 0.5rem;
+    }
+}
+</style>
